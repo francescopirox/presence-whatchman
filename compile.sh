b271c4bef562c9c5df5bfcb481febfa5ce8e3694
J-Lemon <akira.zeta@gmail.com>
2016-11-04 05:07:49 +0100

    Added -O3 flag in the build script

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ *.cpp -o esegui -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_ml -lopencv_video -lopencv_features2d -lopencv_calib3d -lopencv_objdetect -lopencv_contrib -lopencv_legacy -lopencv_stitching
+g++ *.cpp -O3 -o esegui -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_ml -lopencv_video -lopencv_features2d -lopencv_calib3d -lopencv_objdetect -lopencv_contrib -lopencv_legacy -lopencv_stitching
